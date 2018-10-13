8e425ab23aa0b57d79597fe10d2e02d8892b9ac6
Terence Lyu <terencelyu96@gmail.com>
2018-10-13 00:48:35 +0000

    finish sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get  '/signup',  to: 'user#new'
+  post '/signup',  to: 'user#create'
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
