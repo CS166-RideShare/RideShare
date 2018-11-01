8ca4939d84dd2a9fcbb1f36102fdddbc90cba17a
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2018-11-01 10:59:43 -0700

    modified path and add simple_form

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -9,9 +9,20 @@ class RidesController < ApplicationController
   end
 
   def create_request
+    create_params = request_params
+    create_params[:rider_id] = current_user.id
+    @riderequest = Ride.new(create_params)
+    p @riderequest.rider_id
     respond_to do |format|
       format.html
       format.js
     end
   end
+
+  private
+
+    def request_params
+      params.require(:ride).permit(:destination,
+                                   :scheduled_time)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   private
 
     def user_signup_params
