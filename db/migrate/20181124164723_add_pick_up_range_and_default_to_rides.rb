3c26e5f3369e8202546f867c952af131f72dbfb4
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2018-11-27 14:15:51 -0800

    change migration

--- db/migrate/20181124164723_add_pick_up_range_and_default_to_rides.rb
+++ db/migrate/20181124164723_add_pick_up_range_and_default_to_rides.rb
@@ -2,8 +2,9 @@ class AddPickUpRangeAndDefaultToRides < ActiveRecord::Migration[5.2]
   def change
     change_table :rides do |t|
       t.change_default :finished, false
-      t.rename :pickup_time, :pickup_start
-      t.time :pickup_end
+      t.remove :pickup_time
+      t.datetime :pickup_start
+      t.datetime :pickup_end
     end
   end
 end
--- db/migrate/20181124165833_use_date_time_for_pick_up.rb
+++
@@ -1,8 +0,0 @@
-class UseDateTimeForPickUp < ActiveRecord::Migration[5.2]
-  def change
-    change_table :rides do |t|
-      t.change :pickup_start, :datetime
-      t.change :pickup_end, :datetime
-    end
-  end
-end
