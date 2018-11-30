15f5ecfd96d2674f9457e7fcc12b16d203ea0667
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2018-11-30 13:15:26 -0800

    heroku time bug fix

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -83,7 +83,7 @@ class RidesController < ApplicationController
         if time_hash[:day]=="1"
           time += 1.days
         end
-        return Datetime.parse(time.to_s)
+        return DateTime.parse(time.to_s)
       rescue
         return nil
       end
