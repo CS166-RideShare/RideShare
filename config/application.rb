c4bc150fce42e693a079071c31625cb30e5b0abb
QuaternaryKFC <zeng.351@buckeyemail.osu.edu>
2018-12-06 10:33:39 -0800

    ActiveJob after_initialize

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RideShare
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.after_initialize do
+      RequestCleanupJob.perform_now
+    end
   end
 end
